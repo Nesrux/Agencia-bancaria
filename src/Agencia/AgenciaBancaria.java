6794029d927b7d181aee98081355c0c187a6aaa3
Nesrux <joaomarcosdevs@gmail.com>
2023-01-12 20:13:07 -0300

    Metodo criarConta feito

--- src/Agencia/AgenciaBancaria.java
+++ src/Agencia/AgenciaBancaria.java
@@ -25,5 +25,60 @@ public class AgenciaBancaria {
 		System.out.println("|   Opção 4 - Transferir    |");
 		System.out.println("|   Opção 5 - Listar        |");
 		System.out.println("|   Opção 6 - Sair          |");
+		
+		int operacao = entrada.nextInt();
+		switch(operacao) {
+		case 1 :
+			criarConta();
+			break;
+		case 2:
+			depositar();
+			break;
+		case 3:
+			sacar();
+			break;
+		case 4:
+			transferir();
+			break;
+		case 5:
+			listas();
+			break;
+		case 6:
+			System.out.println("Obrigado por usar nossa agencia");
+			System.exit(0);
+		default:
+			System.out.println("Opção Inválida!");
+			operacoes();
+		}
+		
+		
+
 	}
+	
+	public static void criarConta(){
+		System.out.println("\nNome: ");
+		String nome = entrada.nextLine();
+		
+		System.out.println("\nCPF: ");
+		String cpf = entrada.nextLine();
+		
+		System.out.println("\nEmail: ");
+		String email = entrada.nextLine();
+		
+		
+		Cliente cliente = new Cliente(nome, cpf, email);
+		
+		Conta conta = new Conta(cliente);
+		
+		contasBancarias.add(conta);
+		System.out.println("Sua conta foi criada com sucesso!");
+		
+		operacoes();
+	}
+	
+	public static void depositar() {}
+	public static void sacar() {}
+	public static void transferir() {}
+	public static void listas() {}
+	
 }
